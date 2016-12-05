8a37ee7ea0d450d06e53e74de5d920a803c9d8f0
Jonathan Flynn <jonflynndesigner@gmail.com>
2016-12-05 17:33:46 +0000

    Finished Day 5 :D

--- day/day5/day_5.py
+++ day/day5/day_5.py
@@ -16,19 +16,29 @@ from hashlib import md5
 from itertools import takewhile
 
 def solution():
-    salt = "abc"
+    salt = DAY_INPUT[0].strip("\n")
     result = []
     count = 0
     find = "00000"
-    while len(result) < 8:
+    while len(result) < 8 and False:
         hash = md5(salt + str(count)).hexdigest()
-        if not hash.startswith(find):
-            pass
-        else:
+        if hash.startswith(find):
+            print hash
             result.append(hash[5])
-            print result
         count += 1
-    return result, None
+
+    count = 0
+    result2 = [None]*8
+    while any(r is None for r in result2):
+        hash = md5(salt + str(count)).hexdigest()
+        if hash.startswith(find):
+            if hash[5].isdigit():
+                ind = int(hash[5])
+                if ind < 8 and result2[ind] is None:
+                    result2[ind] = hash[6]
+        count += 1
+
+    return ''.join(result), ''.join(result2)
 
 
 ########################################################################################################################
